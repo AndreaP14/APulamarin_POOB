57bacbdeea3f43bdc61cdb0b1da164ab73644693
user <user@LAPTOP-5UGCIOTV>
2021-08-02 18:17:06 -0500

    Primera version de sentencias de control 2DB "B"

---
+++ src/ec/edu/intsuperior/modelo/Cuenta.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author user
+ */
+public class Cuenta {
+
+    private double saldo;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String numcuenta;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumcuenta() {
+        return numcuenta;
+    }
+
+    public void setNumcuenta(String numcuenta) {
+        this.numcuenta = numcuenta;
+    }
+
+    @Override
+    public String toString() {
+        return getNumcuenta()+""+getSaldo(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
